092a8b1d6c5150c591a3e79a4ced188e6d472b5d
JennicaStiehl <jennica.stiehl@gmail.com>
2018-12-11 14:18:39 -0700

    adds refactor_1

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,8 @@ require './lib/cell'
 require './lib/validator'
 
 class Board
+  # Include Validator
+
   attr_reader :cells,
               :number
 
@@ -50,14 +52,14 @@ class Board
 
     alpha = ""
     full_line = ""
-     (1..@number).to_a.each do |num|
+     board_numbers.each do |num|
        string << " #{num}"
      end
      full_line << "#{string} \n"
-    ('A'..('A'.ord + (@number -1)).chr).to_a.each do |letter|
+    board_letters.each do |letter|
        alpha = letter
        string_1 = ""
-      (1..@number).to_a.each do |num|
+      board_numbers.to_a.each do |num|
         key = "#{letter}#{num.to_s}"
          string_1 += "#{@cells[key].render(ship)} "
          # binding.pry
@@ -69,6 +71,14 @@ class Board
 
   def fire_shot(cell)
     @cells[cell].fired_upon
+  end
+
+  def board_letters
+    ('A'..('A'.ord + (number - 1)).chr).to_a
+  end
+
+  def board_numbers
+    (1..@number).to_a
   end
 
 end
